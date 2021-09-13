8a02626f29e43966ac50dfb094cfdd419271e8c3
Arjun <arjun2000raj@gmail.com>
2021-09-13 15:17:24 +0530

    groot translator complete

---
+++ app.js
@@ -0,0 +1,26 @@
+var inputText = document.querySelector('#inputText').value;
+var translateBtn = document.querySelector('#translateBtn');
+var output = document.querySelector('#output');
+
+
+var apiUrl = `https://api.funtranslations.com/translate/groot.json?text=${inputText}`;
+
+const errorHandler = (error) => {
+
+    alert(`You reached Maximum attempts. Please try after an hours.`)
+    console.log(error)
+}
+
+
+const handleClick = () => {
+    
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(json => {
+            output.innerText = json.contents.translated;
+            })
+        .catch(errorHandler)
+}
+
+translateBtn.addEventListener('click', handleClick)
